49ccd218360280eb25256cba6156e9b8bf1ddb15
Sebnem <sebnemgormus@gmail.com>
2022-03-06 16:42:21 +0300

    commit tree 04d90e1153a85dd9ab9e0fdfad3a32a7aa0a06d1

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,17 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { v4 as uuid } from "uuid";
 
 const todosSlice = createSlice({
   name: "todos",
   initialState: {
-    entities: [], // array of todos
+    entities: [], //todos' array
   },
   reducers: {
     todoAdded(state, action) {
-      state.entities.push(action.payload);
+      state.entities.push({
+        id: uuid(), 
+        text: action.payload,
+      });
+    },
+    todoRemoved(state, action) {
+      const index = state.entities.findIndex((todo) => todo.id === action.payload);
+      state.entities.splice(index, 1);
     },
   },
 });
 
-export const { todoAdded } = todosSlice.actions;
+export const { todoAdded, todoRemoved } = todosSlice.actions;
 
 export default todosSlice.reducer;
